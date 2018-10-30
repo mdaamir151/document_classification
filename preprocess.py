e9efea46886c317ecfe1b7bf85c2d0c9bb5db8f2
mdaamir151 <mdaamir151@gmail.com>
2018-10-31 01:52:31 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,5 +2,8 @@
 import _preprocess
 import config
 
-preprocessor = _preprocess.preprocess(config.language_dir)
-preprocessor._toLower(config.language_dir,config.lower_dir)._stem(config.lower_dir,config.stem_dir)._makeNgrams(config.stem_dir,config.grams_dir)._stats(config.grams_dir,config.stats_dir)._splitFiles(config.grams_dir,config.train_dir,10)
+def run(arg,gram_sz=3,split_size=10):
+	if arg == False:
+		return
+	preprocessor = _preprocess.preprocess(config.language_dir,gram_sz)
+	preprocessor._toLower(config.language_dir,config.lower_dir)._stem(config.lower_dir,config.stem_dir)._makeNgrams(config.stem_dir,config.grams_dir)._stats(config.grams_dir,config.stats_dir)._splitFiles(config.grams_dir,config.train_dir,split_size)
